331eb7a73220c4d2941052945b50cb3dd01a1d61
Matt Fung <official.matthiasfung@gmail.com>
2020-09-30 17:48:37 -0700

    Initial upload of Q3 files

--- Assign1.R
+++ Assign1.R
@@ -1,4 +1,42 @@
-data=read.table("Group_Assignment_1_Dataset.txt",header = TRUE,sep=',')
-data$SDate=as.POSIXlt(data$Date,tz="","%d/%m/%Y")
-datasmall<-subset(data, SDate >='2007/07/16' & SDate < '2007/07/23')
+# Import Libraries
+library(ggplot2)
+
+###############################################
+
+# Set the directory
+getwd()
+setwd("C:/Users/offic/Documents/SFU/FALL2020/CMPT318/Assignments/A1")
+
+###############################################
+
+# Load the data
+data = read.table("Group_Assignment_1_Dataset.txt", header = TRUE, sep =',')
+data$SDate = as.POSIXlt(data$Date, tz = "", "%d/%m/%Y")
+
+###############################################
+
+# Dataset for Week 29
+datasmall <- subset(data, SDate >= '2007/07/16' & SDate < '2007/07/23')
 head(datasmall)
+
+###############################################
+
+# Define representative time windows
+#     Day hours (7:30 AM - 5:00 PM)
+#   Night hours (5:00 PM - 7:30 AM)
+
+# Create data sets for representative time windows
+datasmall$date <- strptime(datasmall$date, "%d-%m-%y %H:%M:%S")
+datasmall$date <- as.POSIXct(datasmall$date)
+str(datasmall)
+
+# data set for weekday day
+data_weekday_day <- subset(datasmall, (SDate >= '2007/07/16 07:30:00' & SDate < '2007/07/16 17:00:00') | 
+                                      (SDate >= '2007/07/17 07:30:00' & SDate < '2007/07/17 17:00:00') | 
+                                      (SDate >= '2007/07/18 07:30:00' & SDate < '2007/07/18 17:00:00') |
+                                      (SDate >= '2007/07/19 07:30:00' & SDate < '2007/07/19 17:00:00') |
+                                      (SDate >= '2007/07/20 07:30:00' & SDate < '2007/07/20 17:00:00') )
+head(data_weekday_day)
+
+#myts <- ts(datasmall$Global_intensity, frequency = 24*60, start = 0)
+#plot(myts)
