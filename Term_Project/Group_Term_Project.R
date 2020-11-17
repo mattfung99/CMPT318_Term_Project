dfb4d8cb18f8cec8b6afd42aa18cbe6c16905446
Matt Fung <official.matthiasfung@gmail.com>
2020-11-16 19:28:36 -0800

    Upload of Project Files

---
+++ Term_Project/Group_Term_Project.R
@@ -0,0 +1,108 @@
+### Import Libraries
+library(dplyr)
+library(data.table)
+library(tidyverse)
+library(stats)
+library(ggplot2)
+library(ggbiplot)
+library(depmixS4)
+
+### Set Directory
+getwd()
+setwd("--- Set Your Directory Here ---")
+
+require(tidyverse)
+rawData <- readr::read_csv("TermProjectData.txt")
+rawData[is.na(rawData)] <- 0
+
+###############################################################################################################################################
+# ----------------------------------------------------------------- PART 1---------------------------------------------------------------------
+###############################################################################################################################################
+
+# --------------------------------------------------------- 1. Train and Test HMMS ------------------------------------------------------------
+# 
+#----------------------------------------------------------------Data Cleaning-----------------------------------------------------------------
+
+### Add timestamp to dataframe for graphing
+dateTime <- paste(rawData$Date, rawData$Time)
+rawData$timestamp <- as.POSIXlt(strptime(dateTime, format = "%d/%m/%Y %H:%M:%S"))
+
+### Label all the weeks for 1408
+dateFormat <- as.Date(rawData$Date, format = "%d/%m/%Y")
+rawData$weekNum <- format(dateFormat,"%V")
+
+### Label weekday or weekend
+daysWeek <- format(as.Date(rawData$Date, format = "%d/%m/%Y"),"%u") # days of the week 1-7 (Monday-Sunday)
+rawData$week <- ifelse(daysWeek %in% c("6", "7"), "weekend", "weekdays")
+rawData$dayWeek <- format(as.Date(rawData$Date, format = "%d/%m/%Y"),"%u")
+rawData$year <- as.integer(format(as.Date(rawData$Date, format = "%d/%m/%Y"),"%y"))
+
+### Label time of the day   - morning or evening
+dataTime <- as.ITime(format(rawData$Time, format = "%H:%M:%S"))  #RS added to correct error when run in Linux
+rawData$STime <- as.integer(rawData$Time)  #RS added to correct error when run in Linux
+startTime <- as.ITime("10:30")
+endTime <- as.ITime("13:30")
+rawData$day <- ifelse((dataTime >= startTime & dataTime <= endTime), "window", "other")
+
+### Adjust 0 values in Voltage (they are not meaningful)
+rawData$Voltage[rawData$Voltage < 200.0] = 220.0  
+
+#---------------------------------------------------------------Setup Data for PCA--------------------------------------------------------------
+
+### Create Dataframe for Week 5, Wednesday, 10:30AM - 1:30PM
+filteredData <- filter(rawData, daysWeek == "4", day == "window")
+
+### Calculate Average for every minute during the selected window
+calculateAverage <- function(responseVariable)
+{
+  
+  ### Filter for Global Intensity
+  responseVariableData <- filteredData[, c("timestamp", "STime", responseVariable)]
+  
+  ### Format the time for Week 5
+  responseVariableData$Time <-format(as.POSIXct(responseVariableData$timestamp), format = "%H:%M:%S")
+  responseVariableData$Time <- as.POSIXct(responseVariableData$Time, format = "%H:%M:%S")
+  
+  # Group and average the time periods 
+  avgResponseVariable <- responseVariableData %>%
+    group_by(Time, STime) %>%
+    summarise(avg_value = mean(get(responseVariable)))
+  
+  return(avgResponseVariable)
+}
+
+### Calculate Average for all 7 response variables
+globalIntensityData <- calculateAverage("Global_intensity")
+globalActivePowerData <- calculateAverage("Global_active_power")
+globalReactivePowerData <- calculateAverage("Global_reactive_power")
+voltageData <- calculateAverage("Voltage")
+subMetering1Data <- calculateAverage("Sub_metering_1")
+subMetering2Data <- calculateAverage("Sub_metering_2")
+subMetering3Data <- calculateAverage("Sub_metering_3")
+
+#-------------------------------------------------------------Compute and Plot PCA--------------------------------------------------------------
+
+### Combine the average values from all 7 response variables
+### From Left to Right: Intensity, Active Power, Reactive Power, Voltage, Sub Metering 1, Sub Metering 2, Sub Metering 3
+pcaData <- globalIntensityData[, c("avg_value")]
+pcaData <- cbind(data.frame(pcaData), globalActivePowerData[, c("avg_value")])
+pcaData <- cbind(data.frame(pcaData), globalReactivePowerData[, c("avg_value")])
+pcaData <- cbind(data.frame(pcaData), voltageData[, c("avg_value")])
+pcaData <- cbind(data.frame(pcaData), subMetering1Data[, c("avg_value")])
+pcaData <- cbind(data.frame(pcaData), subMetering2Data[, c("avg_value")])
+pcaData <- cbind(data.frame(pcaData), subMetering3Data[, c("avg_value")])
+
+### Rename Labels
+names(pcaData)[1] <- "Global_intensity"
+names(pcaData)[2] <- "Global_active_power"
+names(pcaData)[3] <- "Global_reactive_power"
+names(pcaData)[4] <- "Voltage"
+names(pcaData)[5] <- "Sub_metering_1"
+names(pcaData)[6] <- "Sub_metering_2"
+names(pcaData)[7] <- "Sub_metering_3"
+
+Test.pca <- prcomp(pcaData, center = TRUE, scale. = TRUE)
+str(Test.pca)
+ggbiplot(Test.pca, scale=.75, alpha = 0.0, var.scale = 1.0) +
+  ggtitle("PCA for 7 Response Variables") + theme(plot.title = element_text(hjust = 0.5))
+summary(Test.pca)
